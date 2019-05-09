33a814e090799f559ce61ac58c52acdbfaf31004
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2019-05-09 14:33:59 -0500

    Passing down props using the context api instead of prop drilling

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Product from './Product.js'
+import Title from "./Title.js"
+import { storeProducts } from "../data.js";
 
 class ProductList extends Component {
+
+    state={
+        products:storeProducts
+    }    
     render(){
+        console.log(this.state.products)
         return(
-        <div>
-            <Product />
-        </div>
+        <React.Fragment>
+            <div className="py-5">
+            <div className="container">
+            <Title name="our" title="product">
+                
+            </Title>
+                <div className="row" />
+                
+                
+            </div>
+            </div>
+        </React.Fragment>
         )
     }
 
